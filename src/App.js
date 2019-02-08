185bb99e10ec1d0ebb859eb780adfc0f978cab71
Trip1701 <bj2336@email.vccs.edu>
2019-02-08 01:16:22 -0500

    Feteched Data Displayed by Browser

--- src/App.js
+++ src/App.js
@@ -16,7 +16,22 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {this.props.data && <h1>Success</h1>}
+        {this.props.data && this.props.data.map( item =>
+                                                    <div>
+                                                      <h1>{item.name}</h1>
+                                                      <img src={item.image} alt={item.name} />
+                                                      <h4>{item.type}</h4>
+                                                      <h2>{`Amiibo Series: ${item.amiiboSeries}`}</h2>
+                                                      <h3>{`Game Series: ${item.gameSeries}`}</h3>
+                                                      <ul>
+                                                        <li>Release: </li>
+                                                        <li>{`Australia: ${item.release.au}`}</li>
+                                                        <li>{`Europe: ${item.release.eu}`}</li>
+                                                        <li>{`Japan: ${item.release.jp}`}</li>
+                                                        <li>{`North America: ${item.release.na}`}</li>
+                                                      </ul>
+
+                                                    </div>)}
       </div>
     );
   }
