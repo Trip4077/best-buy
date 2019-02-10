e43e9de11fc0676109c30fb27f07a7cacc61b8d9
Trip1701 <bj2336@email.vccs.edu>
2019-02-10 03:13:24 -0500

    Panel Toggle and Icons implemented

--- src/components/FilterMenu.js
+++ src/components/FilterMenu.js
@@ -8,8 +8,16 @@ class FilterMenu extends React.Component {
     super(props);
 
     this.state = {
-
+      isOpen: false,
     }
+  }
+
+  togglePanel = e => {
+    this.setState(prevState => {
+      return {
+        isOpen: !prevState.isOpen
+      }
+    })
   }
 
   getKeys = search => {
@@ -31,31 +39,40 @@ class FilterMenu extends React.Component {
   render() {
     const amiiboS = this.getKeys('amiiboSeries');
     const gameS = this.getKeys('gameSeries');
+    let panelStatus;
+    let panelIcon;
+
+    this.state.isOpen ? panelStatus = '' : panelStatus = 'closed';
+    this.state.isOpen ? panelIcon = <i class="fas fa-window-close"></i> : panelIcon = <i class="fas fa-filter"></i>;
+
     return(
-      <section className='filter-menu'>
-        <form className='filter-menu__form'>
-          <div>
-            <h3>Amiibo Series</h3>
-          </div>
+      <section className={`filter-menu ${panelStatus}`}>
+        <span onClick={this.togglePanel} className='icon' >{panelIcon}</span>
+        <div>
+          <form className='filter-menu__form'>
+            <div>
+              <h3>Amiibo Series</h3>
+            </div>
 
-          <ul>
-            <li onClick={this.props.clearFilter}>Clear Filter</li>
-            {amiiboS.map(cat => <li key={Math.random()}
-                                    onClick={this.filterData}> {cat} </li>)}
-          </ul>
-        </form>
+            <ul>
+              <li onClick={this.props.clearFilter}>Clear Filter</li>
+              {amiiboS.map(cat => <li key={Math.random()}
+                                      onClick={this.filterData}> {cat} </li>)}
+            </ul>
+          </form>
 
-        <form className='filter-menu__form'>
-          <div>
-            <h3>Game Series</h3>
-          </div>
+          <form className='filter-menu__form'>
+            <div>
+              <h3>Game Series</h3>
+            </div>
 
-          <ul>
-            <li onClick={this.props.clearFilter}>Clear Filter</li>
-            {gameS.map(cat => <li key={Math.random()}
-                                  onClick={this.filterData}> {cat}</li>)}
-          </ul>
-        </form>
+            <ul>
+              <li onClick={this.props.clearFilter}>Clear Filter</li>
+              {gameS.map(cat => <li key={Math.random()}
+                                    onClick={this.filterData}> {cat}</li>)}
+            </ul>
+          </form>
+        </div>
       </section>
     );
   }
