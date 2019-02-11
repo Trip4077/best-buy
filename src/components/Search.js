b565b2735f6e16766f3d06627568dc6aa7b68aa5
Trip1701 <bj2336@email.vccs.edu>
2019-02-10 21:54:02 -0500

    Search Component State Implemented, Search Term Updates

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,30 @@
 import React from 'react';
 
 class Search extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchTerm: ''
+    }
+  }
+
+  handleChange = e => {
+    e.preventDefault();
+
+    this.setState({
+     searchTerm: e.target.value
+    });
+  }
+
   render() {
     return(
       <form className='search'>
         <input className='search__input'
                type='text'
-               placeholder='Search by name...' />
+               placeholder='Search by name...'
+               onChange={this.handleChange}
+               value={this.state.searchTerm} />
 
         <button className='search__btn'>Search</button>
       </form>
